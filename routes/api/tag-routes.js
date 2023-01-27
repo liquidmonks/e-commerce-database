39d46d7573428a9fe283c97d6fb33b6dda28d082
Liquid Monks <info@liquidmonks.com>
2023-01-27 11:01:47 -0600

    tag-routes implements the get, post and put endpoints.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,33 +1,47 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   const data = await Tag.findAll({
-    include: [
-      {model: Product, through: ProductTag}
-    ],
+    include: [{ model: Product, through: ProductTag }],
   });
   // be sure to include its associated Products
   return res.status(200).json(data);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const data = await Tag.findOne({
+    where: { id: req.params.id },
+    include: [Product],
+  });
+  return res.status(200).json(data);
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const data = await Tag.create(req.body);
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.json(err);
+  }
+  // console.log(req.body);
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  const data = await Tag.update(req.body, {
+    where: { id: req.params.id },
+  });
+  return res.status(200).json(data);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   const data = await Tag.destroy({
     where: { id: req.params.id },
